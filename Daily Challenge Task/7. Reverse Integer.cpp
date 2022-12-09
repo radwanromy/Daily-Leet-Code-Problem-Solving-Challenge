0e383d80e5d09d0067ee7c6db10f63c51f5e5402
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-12-09 18:18:18 +0600

    LC daily challenge 17

---
+++ Daily Challenge Task/7. Reverse Integer.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+7. Reverse Integer
+Medium
+
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+*******************************************************************************/
+class Solution {
+public:
+    int reverse(int x) {
+        int ret = 0;
+        while (x != 0) {
+            if (abs(ret) > 214748364) return 0;
+            ret = ret * 10 + x % 10;
+            x /= 10;
+        }
+        return ret;
+    }
+};
